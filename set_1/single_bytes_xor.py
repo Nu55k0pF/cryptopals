0d6fcac385c7fc96695eb53bbe981466823d2c99
Nu55k0pF <madironbrain@googlmail.com>
2022-01-19 22:45:15 +0100

    Got the script into a state, where all messages possible messages get decrypted and fixed encoding errors

---
+++ set_1/single_bytes_xor.py
@@ -0,0 +1,44 @@
+# https://gist.github.com/mikeecb/0d75f46521fe526a0138ae5265392505
+message = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+bmessage = bytearray.fromhex(message)
+
+def generate_keys(msg):
+    keys = []
+    for i in range(256):
+        keys.append(bytearray(([i]) * len(msg)))
+    return keys
+
+def xor(bar1, bar2):
+    return bytes(a ^ b for a, b in zip(bar1, bar2))
+
+def single_byte_xor(msg):
+    b_msg = bytearray.fromhex(msg)
+    plain_texts = []
+    keys = generate_keys(msg)
+    for key in keys:
+        plain_texts.append(xor(b_msg, key).decode(encoding='ASCII', errors='ignore'))
+    return plain_texts
+
+def get_lcount(text):
+    """count all letters in text"""
+    letter_count = {
+    'a': 0,    'b': 0,    'c': 0,    'd': 0,
+    'e': 0,    'f': 0,    'g': 0,    'h': 0,
+    'i': 0,    'j': 0,    'k': 0,    'l': 0,
+    'm': 0,    'n': 0,    'o': 0,    'p': 0,
+    'q': 0,    'r': 0,    's': 0,    't': 0,
+    'u': 0,    'v': 0,    'w': 0,    'x': 0,
+    'y': 0,    'z': 0,    ' ': 0
+    }
+
+    for letter in text:
+        if str.lower(letter) in letter_count:
+            letter_count[str.lower(letter)] += 1
+        else:
+            pass
+    return letter_count
+
+
+plain_texts = single_byte_xor(message)
+for text in plain_texts:
+    print(text)
