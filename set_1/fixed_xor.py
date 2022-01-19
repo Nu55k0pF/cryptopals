a8811755be086b52cbed322f7cec14447d7082aa
Nu55k0pF <madironbrain@googlmail.com>
2022-01-19 23:15:44 +0100

    changed bytres to bytarray

--- set_1/fixed_xor.py
+++ set_1/fixed_xor.py
@@ -4,8 +4,8 @@ k = "686974207468652062756c6c277320657965"
 def fixed_xor(chrstr1, chrstr2):
     """Takes two charachter strings of the same length and xors them against each other"""
     if len(chrstr1) == len(chrstr2):
-        bar1 = bytes.fromhex(chrstr1)
-        bar2 = bytes.fromhex(chrstr2)
+        bar1 = bytearray.fromhex(chrstr1)
+        bar2 = bytearray.fromhex(chrstr2)
         return bytes(a ^ b for a, b in zip(bar1, bar2)).hex()
     else:
         print("Message and key must be of same length")
